8c8f0a977840e000801bb1db84b527a6f873bc78
story6451 <garan.cw@gmail.com>
2023-09-03 21:26:25 +0100

    fuck

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include <Servo.h>
 
 const float AltitudeUncertainty = 1;
-const int Servo1Offset = 0, Servo2Offset = 0, Servo3Offset = 0, Servo4Offset = 0;
+const int8_t Servo1Offset = 15, Servo2Offset = 5, Servo3Offset = 5, Servo4Offset = -2;
 
 Adafruit_BMP280 BMPSensor;
 File FileHandler;
@@ -17,6 +17,8 @@ Servo Servo2; //.
 Servo Servo3; //.
 Servo Servo4; //.. control servos
 Servo Servo5; //recovery system servo
+String Data = "";
+uint64_t timer = 0;
 
 float AngleRoll = 0, AnglePitch = 0, AngleYaw = 0;
 
@@ -29,7 +31,7 @@ float Temperature = 0;
 float Pressure = 0;
 bool FilePresent = true;
 bool Launched = false;
-int MainStartTime = 0;
+uint64_t MainStartTime = 0;
 
 void SetupSensors()
 {
@@ -68,6 +70,7 @@ void SetupSensors()
   if (FileHandler)
   {
     FileHandler.print("Starting Altitude/m: "); FileHandler.println(StartingAltitude);
+    Serial.println("Starting alt written to file");
   }
   else
   {
@@ -89,20 +92,20 @@ void SetupServos()
   delay(1000);
 
   // quickly tests each servo to make sure visibly that their power source is sufficient
-  Servo1.write(0);
-  Servo2.write(0);
-  Servo3.write(0);
-  Servo4.write(0);
+  Servo1.write(0 - Servo1Offset);
+  Servo2.write(0 + Servo2Offset);
+  Servo3.write(0 - Servo3Offset);
+  Servo4.write(0 + Servo4Offset);
   delay(250);
-  Servo1.write(180);
-  Servo2.write(180);
-  Servo3.write(180);
-  Servo4.write(180);
+  Servo1.write(180 - Servo1Offset);
+  Servo2.write(180 + Servo2Offset);
+  Servo3.write(180 - Servo3Offset);
+  Servo4.write(180 + Servo4Offset);
   delay(250);
-  Servo1.write(90);
-  Servo2.write(90);
-  Servo3.write(90);
-  Servo4.write(90);
+  Servo1.write(90 - Servo1Offset);
+  Servo2.write(90 + Servo2Offset);
+  Servo3.write(90 - Servo3Offset);
+  Servo4.write(90 + Servo4Offset);
   delay(250);
 }
 
@@ -118,9 +121,9 @@ void RetrieveGyroData()
 {
   //updates gyro and gets angles
   Gyroscrope.update();
-  AngleRoll = Gyroscrope.getAngleX();
+  AngleRoll = Gyroscrope.getAngleZ();
   AnglePitch = Gyroscrope.getAngleY();
-  AngleYaw = Gyroscrope.getAngleZ();
+  AngleYaw = Gyroscrope.getAngleX();
 }
 
 void RetrieveBMPSensorData()
@@ -141,31 +144,39 @@ void DeployRecoverySystem()
 void LogData()
 {
   //writes all relavent variables to a file
-  FileHandler = SD.open("DATA.txt", FILE_WRITE);
-  if (FileHandler)
+  if ((millis() - timer) > 200)
   {
-    FileHandler.print("Time/ms: "); FileHandler.print(millis()); FileHandler.print(" ");
-    FileHandler.print("Alt/m: "); FileHandler.print(Altitude); FileHandler.print(" ");
-    FileHandler.print("Delta Alt/m: "); FileHandler.print(DeltaAltitude); FileHandler.print(" ");
-    FileHandler.print("Max Alt/m: "); FileHandler.print(MaxAltitude); FileHandler.print(" ");
-    FileHandler.print("Delta Max Alt/m: "); FileHandler.print(DeltaMaxAltitude); FileHandler.print(" ");
-    FileHandler.print("Temp/'C: "); FileHandler.print(Temperature); FileHandler.print(" ");
-    FileHandler.print("Pre/hPa: "); FileHandler.print(Pressure); FileHandler.print(" ");
-    FileHandler.print("Yaw/' "); FileHandler.print(AngleYaw); FileHandler.print(" ");
-    FileHandler.print("Pitch/': "); FileHandler.print(AnglePitch); FileHandler.print(" ");
-    FileHandler.print("Roll/': "); FileHandler.print(AngleRoll); FileHandler.println(" ");
+    FileHandler = SD.open("DATA.txt", FILE_WRITE);
+    if (FileHandler)
+    {
+      Serial.println(" Data was written ");
+      FileHandler.print("Time/ms: "); FileHandler.print(millis()); FileHandler.print(" ");
+      FileHandler.print("Alt/m: "); FileHandler.print(Altitude); FileHandler.print(" ");
+      FileHandler.print("Delta Alt/m: "); FileHandler.print(DeltaAltitude); FileHandler.print(" ");
+      FileHandler.print("Max Alt/m: "); FileHandler.print(MaxAltitude); FileHandler.print(" ");
+      FileHandler.print("Delta Max Alt/m: "); FileHandler.print(DeltaMaxAltitude); FileHandler.print(" ");
+      FileHandler.print("Temp/'C: "); FileHandler.print(Temperature); FileHandler.print(" ");
+      FileHandler.print("Pre/hPa: "); FileHandler.print(Pressure); FileHandler.print(" ");
+      FileHandler.print("Yaw/' "); FileHandler.print(AngleYaw); FileHandler.print(" ");
+      FileHandler.print("Pitch/': "); FileHandler.print(AnglePitch); FileHandler.print(" ");
+      FileHandler.print("Roll/': "); FileHandler.print(AngleRoll); FileHandler.println(" ");
+    }
+    else
+    {
+      Serial.print(" Failed to write data ");
+      FilePresent = false;
+    }
     FileHandler.close();
-    Serial.print("Data was written ");
-  }
-  else
-  {
-    Serial.print("Failed to write data ");
-    FilePresent = false;
+    timer = millis();  
   }
 }
 
 void OutputOnSerial()
 {
+
+  Serial.print("accX/g "); Serial.print(Gyroscrope.getAccX()); Serial.print(" ");
+  Serial.print("accY/g "); Serial.print(Gyroscrope.getAccY()); Serial.print(" ");
+  Serial.print("accZ/g "); Serial.print(Gyroscrope.getAccZ()); Serial.print(" ");
   Serial.print("Time/ms: "); Serial.print(millis()); Serial.print(" ");
   Serial.print("Alt/m: "); Serial.print(Altitude); Serial.print(" ");
   Serial.print("Delta Alt/m: "); Serial.print(DeltaAltitude); Serial.print(" ");
@@ -173,9 +184,6 @@ void OutputOnSerial()
   Serial.print("Delta Max Alt/m: "); Serial.print(DeltaMaxAltitude); Serial.print(" ");
   Serial.print("Temp/'C: "); Serial.print(Temperature); Serial.print(" ");
   Serial.print("Pre/hPa: "); Serial.print(Pressure); Serial.print(" ");
-  Serial.print("accX/g "); Serial.print(Gyroscrope.getAccX()); Serial.print(" ");
-  Serial.print("accY/g "); Serial.print(Gyroscrope.getAccY()); Serial.print(" ");
-  Serial.print("accZ/g "); Serial.print(Gyroscrope.getAccZ()); Serial.print(" ");
   Serial.print("Yaw/' "); Serial.print(AngleYaw); Serial.print(" ");
   Serial.print("Pitch/': "); Serial.print(AnglePitch); Serial.print(" ");
   Serial.print("Roll/': "); Serial.print(AngleRoll); Serial.println(" ");
@@ -186,17 +194,16 @@ void loop()
   RetrieveGyroData();
   if (Launched == true)
   {
-    Servo1.write(map(AnglePitch, -180, 180, 0, 180) + Servo1Offset);
+    Servo1.write(map(AnglePitch, -180, 180, 0, 180) - Servo1Offset);
     Servo2.write(map(AnglePitch, -180, 180, 180, 0) + Servo2Offset);
-    Servo3.write(map(AngleYaw, -180, 180, 0, 180) + Servo3Offset);
-    Servo4.write(map(AngleYaw, -180, 180, 180, 0) + Servo4Offset);
+    Servo3.write(map(AngleYaw, -180, 180, 180, 0) - Servo3Offset);
+    Servo4.write(map(AngleYaw, -180, 180, 0, 180) + Servo4Offset);
 
     if (Altitude > MaxAltitude)
     {
       MaxAltitude = Altitude;
       DeltaMaxAltitude = MaxAltitude - StartingAltitude;
     }
-
     //checks to see if the altitude is decreasing
     if ((DeltaAltitude < (DeltaMaxAltitude - AltitudeUncertainty)) || ((millis() - MainStartTime) > 3500))
     {
